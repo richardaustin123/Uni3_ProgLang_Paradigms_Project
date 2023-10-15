19908efc93b0b72c92347289e34237d8768dfc18
Richard Austin <richardaustin123@icloud.com>
2023-10-15 15:29:35 +0100

    naming cleanup + w/f spread implemented
    Cleaned up names of variables and methods to fit proper schemes
    
    Implemented system of water flow and fire spread where if you select water and room above or below is also water, spread water down to all rooms directly below and opposite for fire

--- 20032144/Python/python_code.py
+++ 20032144/Python/python_code.py
@@ -20,8 +20,8 @@
 # [ 0, 1, 1, 1, 0]]    # 1.  2.  3.  4.  5.
 
 # Globle variables
-playerOneWinner = False
-playerTwoWinner = False
+playerOneWins = False
+playerTwoWins = False
 playerOnesGo = True
 playerTwosGo = False
 gameOver = False
@@ -40,84 +40,15 @@ playerTwoBuilding = [[ 1, 0, 0, 0, 1],    # 16. 17. 18. 19. 20.
                      [ 0, 1, 0, 1, 0],    # 6.  7.  8.  9.  10.
                      [ 0, 0, 1, 0, 0]]    # 1.  2.  3.  4.  5.
 
-# Set the building
-building = ["-"] * 20
+# main()
+def main():
+    play()
 
-# display_playerone_building()
-# Use the previously made building array of "-"s to display the building
-def display_playerone_building():
-    print("\nPlayer 1's building")
-    for row in playerOneBuilding:
-        print(' '.join(map(str, row)))
-    print("\n")
-
-# display_playertwo_building()
-# Use the previously made building array of "-"s to display the building
-def display_playertwo_building():
-    print("Player 2's building")
-    for row in playerTwoBuilding:
-        print(' '.join(map(str, row)))
-    print("\n")
-
-# playerOneWin()
-def playerOneWin(playerOneWinner):
-    if playerOneBuilding == [[ 1, 1, 1, 1, 1],
-                             [ 1, 1, 1, 1, 1],
-                             [ 1, 1, 1, 1, 1],
-                             [ 1, 1, 1, 1, 1]] or playerTwoBuilding == [[ 0, 0, 0, 0, 0],
-                                                                        [ 0, 0, 0, 0, 0],
-                                                                        [ 0, 0, 0, 0, 0], 
-                                                                        [ 0, 0, 0, 0, 0]]:
-        playerOneWinner = True
-        return playerOneWinner
-
-# playerTwoWin()
-def playerTwoWin(playerTwoWinner):
-    if playerTwoBuilding == [[ 1, 1, 1, 1, 1],
-                             [ 1, 1, 1, 1, 1],
-                             [ 1, 1, 1, 1, 1],
-                             [ 1, 1, 1, 1, 1]] or playerOneBuilding == [[ 0, 0, 0, 0, 0],
-                                                                        [ 0, 0, 0, 0, 0],
-                                                                        [ 0, 0, 0, 0, 0], 
-                                                                        [ 0, 0, 0, 0, 0]]:
-        playerTwoWinner = True
-        return playerTwoWinner
-
-# checkWinner()
-def checkWinner(gameOver, playerOneWinner, playerTwoWinner):
-    if playerOneWin(playerOneWinner) == True:
-        print("Player 1 wins\n")
-        gameOver = True
-    elif playerTwoWin(playerTwoWinner) == True:
-        print("Player 2 wins\n")
-        gameOver = True
-    return gameOver
-
-# take_turn(+player)
-def take_turn(player, row, col):
-    print("It's player " + player + "'s turn\n")
-    print("Press 1 to save your building or press 0 to spread fire on your opponent...\n")
-    waterOrFire = int(input("Enter 1 or 0: \n")) 
-    move = int(input("Which room do you chose (1-20): \n")) # get the move index position
-    if player == "1" and waterOrFire == 1:
-        row, col = update_building(move, row, col)
-        playerOneBuilding[row][col] = 1 # User enters 1-20 where 1 is the bottom of the building and 20 is the top so we need to reverse the index (19 - flat number) and then - 1 as array is 0-19 not 1-20
-    elif player == "1" and waterOrFire == 0:
-        row, col = update_building(move, row, col)
-        playerTwoBuilding[row][col] = 0
-    elif player == "2" and waterOrFire == 1:
-        row, col = update_building(move, row, col)
-        playerTwoBuilding[row][col] = 1
-    elif player == "2" and waterOrFire == 0:
-        row, col = update_building(move, row, col)
-        playerOneBuilding[row][col] = 0
-
-# update_building(+move, -row, -col)
-def update_building(move, row, col):
-    if 1 <= move <= 20:
-        row = 3 - (move - 1) // 5
-        col = (move - 1) % 5
-    return row, col
+# play()
+# Kick off the game
+def play():
+    intro()
+    game_loop()
 
 # intro()
 def intro():
@@ -144,33 +75,144 @@ def display_building_numbers():
 
 # game_loop()
 def game_loop():
-    global playerOneWinner, playerTwoWinner, playerOnesGo, playerTwosGo, gameOver, row, col
-    while playerOneWinner == False and playerTwoWinner == False and gameOver == False:
-        if gameOver == False:
-            display_playerone_building()
-            display_playertwo_building()
-            if playerOnesGo == True:
-                take_turn("1", row, col)
-                gameOver = checkWinner(gameOver, playerOneWinner, playerTwoWinner)
-                playerOnesGo = False
-                playerTwosGo = True
-            elif playerTwosGo == True:
+    global playerOneWins, playerTwoWins, playerOnesGo, playerTwosGo, gameOver, row, col # Global variables 
+    while playerOneWins == False and playerTwoWins == False and gameOver == False:      # While no one has won
+        if gameOver == False:                                                               # If the game is not over
+            display_both_buildings()                                                        # Display player one and two's buildings
+            if playerOnesGo == True:                                                        # If it's player one's go   
+                take_turn("1", row, col)                                                    # Take player one's turn
+                gameOver = check_winner(gameOver, playerOneWins, playerTwoWins)          # Check if player one has won
+                playerOnesGo = False                                                        # Set player one's go to false
+                playerTwosGo = True                                                         # Set player two's go to true
+            elif playerTwosGo == True:                                                      # Same as above but for player two
                 take_turn("2", row, col)
-                gameOver = checkWinner(gameOver, playerOneWinner, playerTwoWinner)
+                gameOver = check_winner(gameOver, playerOneWins, playerTwoWins)
                 playerTwosGo = False
                 playerOnesGo = True
         elif gameOver == True:
             print("Game over\n")
 
-# play()
-# Kick off the game
-def play():
-    intro()
-    game_loop()
+# display_playerone_building()
+# Print player one's building
+def display_playerone_building():
+    print("\nPlayer 1's building")
+    for row in playerOneBuilding:
+        print(' '.join(map(str, row)))
+    print("\n")
 
-# main()
-def main():
-    play()
+# display_playertwo_building()
+# Print player two's building
+def display_playertwo_building():
+    print("Player 2's building")
+    for row in playerTwoBuilding:
+        print(' '.join(map(str, row)))
+    print("\n")
+
+# display_both_buildings()
+def display_both_buildings():
+    display_playerone_building()
+    display_playertwo_building()
+
+# take_turn(+player, +row, +col)
+# Each player takes a turn and their building is updated
+def take_turn(player, row, col):
+    print("It's player " + player + "'s turn\n")
+    print("Press 1 to save your building or press 0 to spread fire on your opponent...\n")
+    waterOrFire = int(input("Enter 1 or 0: \n")) 
+    roomNumber = int(input("Which room do you chose (1-20): \n")) # get the room number index position
+    if player == "1" and waterOrFire == 1:
+        row, col = update_building(roomNumber, row, col) # Get the row and column to be updated in the building array, based on the room number selected by the user
+        playerOneBuilding[row][col] = 1
+        above_and_below(player, waterOrFire, row, col)
+    elif player == "1" and waterOrFire == 0:
+        row, col = update_building(roomNumber, row, col)
+        playerTwoBuilding[row][col] = 0
+        above_and_below(player, waterOrFire, row, col)
+    elif player == "2" and waterOrFire == 1:
+        row, col = update_building(roomNumber, row, col)
+        playerTwoBuilding[row][col] = 1
+        above_and_below(player, waterOrFire, row, col)
+    elif player == "2" and waterOrFire == 0:
+        row, col = update_building(roomNumber, row, col)
+        playerOneBuilding[row][col] = 0
+        above_and_below(player, waterOrFire, row, col)
+
+# update_building(+roomNumber, -row, -col)
+# Get the row and column that needs to be updated in the building array, based on the room number selected by the user
+def update_building(roomNumber, row, col):
+    if 1 <= roomNumber <= 20:
+        row = 3 - (roomNumber - 1) // 5   # e.g. roomNumber = 13 so 13 - 1 = 12 // 5 = 2 so row = 3 - 2 = 1 which is the second row (0, 1, 2, 3)
+        col = (roomNumber - 1) % 5        # e.g. roomNumber = 13 so 13 - 1 = 12 % 5 = 2 so col = 2 which is the third column (0, 1, 2, 3, 4)
+    return row, col
+
+# above_and_below(+player, +waterOrFire, +row, +col)
+# Spread water or fire (1 or 0) below or above the room selected by the user, if the toom below or above is already water or fire (1 or 0)
+def above_and_below(player, waterOrFire, row, col):
+    # As long as the user doesn't select water for the bottom row or fire for the top row
+    # As water cant spread to the bottom if we're already at the bottom
+    # and fire cant spread to the top if we're already at the top
+    if not(waterOrFire == 1 and row == 3) or (waterOrFire == 0 and row == 0):
+        # If p1 selects 1 (water) and the row below is 1 (water) then set all rows below to 1 (water)
+        if ((player == "1" and waterOrFire == 1) and (playerOneBuilding[row + 1][col] == 1 or playerOneBuilding[row - 1][col] == 1)):
+            # set all rows below row+1 to 1 in a loop
+            for i in range(row + 1, len(playerOneBuilding)):
+                playerOneBuilding[i][col] = 1
+        # If p1 selects 0 (fire) and the row above is 0 (fire) then set all rows above to 0 (fire)
+        elif ((player == "1" and waterOrFire == 0) and (playerTwoBuilding[row + 1][col] == 0 or playerTwoBuilding[row - 1][col] == 0)):
+            # set all rows above row-1 to 0 in a loop
+            for i in range(row - 1, -1, -1):
+                playerTwoBuilding[i][col] = 0
+        # If p2 selects 1 (water) and the row below is 1 (water) then set all rows below to 1 (water)
+        elif ((player == "2" and waterOrFire == 1) and (playerTwoBuilding[row + 1][col] == 1 or playerTwoBuilding[row - 1][col] == 1)):
+            # set all rows below row+1 to 1 in a loop
+            for i in range(row + 1, len(playerTwoBuilding)):
+                playerTwoBuilding[i][col] = 1
+        # If p2 selects 0 (fire) and the row above is 0 (fire) then set all rows above to 0 (fire)
+        elif ((player == "2" and waterOrFire == 0) and (playerOneBuilding[row + 1][col] == 0 or playerOneBuilding[row - 1][col] == 0)):
+            # set all rows above row-1 to 0 in a loop
+            for i in range(row - 1, -1, -1):
+                playerOneBuilding[i][col] = 0
+
+
+# check_winner(-gameOver, +playerOneWins, +playerTwoWins)
+# Check if p1 or p2 has won, return gameOver
+def check_winner(gameOver, playerOneWins, playerTwoWins):
+    if check_player_one_win(playerOneWins) == True:
+        gameOver = True
+        display_both_buildings()
+        print("Player 1 wins\n")
+    elif check_player_two_win(playerTwoWins) == True:
+        gameOver = True
+        display_both_buildings()
+        print("\nPlayer 2 wins\n")
+    return gameOver
+
+# check_player_one_win(-playerOneWins)
+# If player one's building has no fire (all 1's) or player two's building is all fire (all 0's) then player one wins
+def check_player_one_win(playerOneWins):
+    if playerOneBuilding == [[ 1, 1, 1, 1, 1],
+                             [ 1, 1, 1, 1, 1],
+                             [ 1, 1, 1, 1, 1],
+                             [ 1, 1, 1, 1, 1]] or playerTwoBuilding == [[ 0, 0, 0, 0, 0],
+                                                                        [ 0, 0, 0, 0, 0],
+                                                                        [ 0, 0, 0, 0, 0], 
+                                                                        [ 0, 0, 0, 0, 0]]:
+        playerOneWins = True
+        return playerOneWins
+
+# check_player_two_win(-playerTwoWins)
+# If player two's building has no fire (all 1's) or player one's building is all fire (all 0's) then player two wins
+def check_player_two_win(playerTwoWins):
+    if playerTwoBuilding == [[ 1, 1, 1, 1, 1],
+                             [ 1, 1, 1, 1, 1],
+                             [ 1, 1, 1, 1, 1],
+                             [ 1, 1, 1, 1, 1]] or playerOneBuilding == [[ 0, 0, 0, 0, 0],
+                                                                        [ 0, 0, 0, 0, 0],
+                                                                        [ 0, 0, 0, 0, 0], 
+                                                                        [ 0, 0, 0, 0, 0]]:
+        playerTwoWins = True
+        return playerTwoWins
+
 
 # Run the main function
 # Environment variables
