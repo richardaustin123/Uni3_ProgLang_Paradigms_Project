6a415860c1cbd4790c99259b29aefce521142158
Richard Austin <richardaustin123@icloud.com>
2023-10-24 15:35:29 +0100

    C++ tictactoe + fizzbuzz

--- 20032144/Prolog/replace_element.pl
+++ 20032144/Prolog/replace_element.pl
@@ -39,6 +39,8 @@ intro :-
     format('~nPress enter to start~n'),
     get_char(_).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Display buildings %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 % display_both_buildings/0
 % Print the building of both players
 display_both_buildings :-
@@ -74,25 +76,91 @@ display_building_numbers :-
     format(' 6.   7.   8.   9.   10.~n'),
     format(' 1.   2.   3.   4.   5.~n').
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Game loop %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 % game_loop/0
 game_loop :-
     format('~nLets begin~n'),
     buildingOne(BuildingOne),
     buildingTwo(BuildingTwo),
     display_both_buildings, 
-    % check_winner(BuildingOne, BuildingTwo), !,
+    check_winner(BuildingOne, BuildingTwo), !,
     whos_turn(Player),
-    take_turn(Player, BuildingOne, BuildingTwo).
-
-game_loop :-
+    take_turn(Player, BuildingOne, BuildingTwo),
     game_loop.
 
-% Test cases
-test :-
-    buildingOne(BuildingOne),
-    buildingTwo(BuildingTwo),
-    whos_turn(Player),
-    take_turn(Player, BuildingOne, BuildingTwo).
+% game_loop :-
+%     game_loop.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Check winner %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%! check_winner(+PlayerOneBuilding, +PlayerTwoBuilding)
+% Check if player one has won 
+% If fail, check_winner will run the next attempt below, for p2
+check_winner(PlayerOneBuilding, PlayerTwoBuilding) :-
+    check_player_one_win(PlayerOneBuilding, PlayerTwoBuilding),
+    format('Player 1 wins~n'),
+    halt.
+
+% Check if player two has won
+% If fail, check_winner will fail and the game will continue on the next attempt
+check_winner(PlayerOneBuilding, PlayerTwoBuilding) :-
+    check_player_two_win(PlayerOneBuilding, PlayerTwoBuilding),
+    format('Player 2 wins~n'), 
+    halt.
+
+% If neither player has won, the game will continue
+check_winner(_PlayerOneBuilding, _PlayerTwoBuilding) :- !.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Check player win %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%! check_player_one_win(+PlayerOneBuilding, +PlayerTwoBuilding)
+% Check if player one has won by extinguishing their own building 
+check_player_one_win(PlayerOneBuilding, _PlayerTwoBuilding) :-
+    building_extinguished(PlayerOneBuilding).
+
+% Check if player one has won by burning down their opponents building
+check_player_one_win(_PlayerOneBuilding, PlayerTwoBuilding) :-
+    building_in_flames(PlayerTwoBuilding).
+
+%! check_player_two_win(+PlayerOneBuilding, +PlayerTwoBuilding)
+% Check if player two has won by extinguishing their own building
+check_player_two_win(_PlayerOneBuilding, PlayerTwoBuilding) :-
+    building_extinguished(PlayerTwoBuilding).
+
+% Check if player two has won by burning down their opponents building
+check_player_two_win(PlayerOneBuilding, _PlayerTwoBuilding) :-
+    building_in_flames(PlayerOneBuilding).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Building put out or on fire %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%! is_room_water(+buildingArray)
+% Check if building is all 1's (water)
+is_room_water([]).                          % Base case
+is_room_water([1 | Rest]) :-                % If the current room is 1 (water)
+    is_room_water(Rest).                    % Loop back to check the rest of the building
+
+% Check if a matrix contains all ones
+building_extinguished([]).                  % Base case
+building_extinguished([Row | Rest]) :-      % Separate the matrix (building) into the current row and the rest of the matrix
+    is_room_water(Row),                     % Check if the current row is all 1's (water)
+    building_extinguished(Rest).            % Loop back to check the rest of the building
+
+%! is_room_fire(+buildingArray)
+% Check if building is all 0's (fire)
+is_room_fire([]).                           % Same as above but for 0's (fire)
+is_room_fire([0 | Rest]) :- 
+    is_room_fire(Rest).
+
+% Check if a matrix contains all ones
+building_in_flames([]).
+building_in_flames([Row | Rest]) :-
+    is_room_fire(Row),
+    building_in_flames(Rest).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Take turns %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %! whos_turn(-Player)
 % Check whos turn it is
@@ -106,6 +174,7 @@ switch_turn :-
     NextPlayer is 3 - Player,           % Switch between 1 and 2 
     assert(current_turn(NextPlayer)).   % Assert (store) the new player to memory
 
+%! take_turn(+Player, +BuildingOne, +BuildingTwo)
 take_turn(Player, BuildingOne, BuildingTwo) :-
     format('~nIts player ~ws turn~n', [Player]),
     format('BuildingOne before: ~w~n', [BuildingOne]),
@@ -120,72 +189,54 @@ take_turn(Player, BuildingOne, BuildingTwo) :-
     get_char(RoomNumberChar),
     atom_number(RoomNumberChar, RoomNumber),
     update_building(RoomNumber, Row, Col),
-    player_turn(FireOrWater, BuildingOne, BuildingTwo, Row, Col),
-    display_both_buildings.
+    player_turn(FireOrWater, BuildingOne, BuildingTwo, Row, Col).
     % spread_water(BuildingOne, 1, 1, NewBuilding), format('BuildingOne after: ~w~n', [NewBuilding]),
     % spread_fire(BuildingTwo, 3, 0, BrandNewBuilding), format('BuildingTwo after: ~w~n', [BrandNewBuilding]).
 
+%! chose_fire_or_water(-FireOrWater)
 chose_fire_or_water(FireOrWater) :-
     format('Input 0 to spread fire and 1 to spead water: ~n'),
     get_char(FireOrWaterChar),
     atom_number(FireOrWaterChar, FireOrWater).
 
+%! chose_room_number(-RoomNumber)
 chose_room_number(RoomNumber) :-
     format('~nChoose a room to spread to: ~n'),
     get_char(RoomNumberChar),
     atom_number(RoomNumberChar, RoomNumber).
 
+%! update_building(+RoomNumber, -Row, -Col)
+% Update the row and column of the room number the player chose
 update_building(RoomNumber, Row, Col) :-
     Row is 3 - (RoomNumber - 1) // 5,
     Col is (RoomNumber - 1) mod 5.
 
+%! player_turn(+FireOrWater, +BuildingOne, +BuildingTwo, +Row, +Col)
+% Player chose to spread fire
 player_turn(0, _, BuildingTwo, Row, Col) :-
     spread_fire(BuildingTwo, Row, Col, BrandNewBuilding),
     format('~nBuildingTwo after: ~w~n', [BrandNewBuilding]).
 
+% Player chose to spread water
 player_turn(1, BuildingOne, _, Row, Col) :-
     spread_water(BuildingOne, Row, Col, NewBuilding),
     format('~nBuildingOne after: ~w~n', [NewBuilding]).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Spread fire and water %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%! spread_water(+Building, +Row, +Col, -NewBuilding)
 % If on the bottom row, this predicate will run and replace only the room they chose
 spread_water(Building, Row, Col, NewBuilding) :-
     check_already_on_bottom_row(Row),
     replace([Row, Col], Building, 1, NewBuilding).
 
+%! spread_water(+Building, +Row, +Col, -NewBuilding)
 % Recursive case: Spread water to the room below and continue recursion
 spread_water(Building, Row, Col, NewBuilding) :-
     not(check_already_on_bottom_row(Row)), % Check if we're not on the bottom row
     replace([Row, Col], Building, 1, IntermediateBuilding), % Spread water to the current room
     NextRow is Row + 1,
     spread_water(IntermediateBuilding, NextRow, Col, NewBuilding).
-
-
-%! check_already_on_bottom_row(+Row)
-% Check if the room is on the bottom row
-% Cant spread water down if already on the bottom row
-check_already_on_bottom_row(Row) :-
-    Row = 3.
-
-%! replace(+[RowIndex, ColIndex], +Building, +ReplaceWith, -NewBuilding)
-replace([RowIndex, ColIndex], Building, ReplaceWith, NewBuilding) :-
-    replace_in_row(RowIndex, ColIndex, Building, ReplaceWith, NewBuilding).
-
-%! replace_in_row(+RowIndex, +ColIndex, +Building, +ReplaceWith, -NewBuilding)
-replace_in_row(0, ColIndex, [Row|Rest], ReplaceWith, [NewRow|Rest]) :-
-    replace_in_col(ColIndex, Row, ReplaceWith, NewRow).
-
-replace_in_row(RowIndex, ColIndex, [Row|Rest], ReplaceWith, [Row|NewRest]) :-
-    RowIndex > 0,
-    NextRowIndex is RowIndex - 1,
-    replace_in_row(NextRowIndex, ColIndex, Rest, ReplaceWith, NewRest).
-
-%! replace_in_col(+ColIndex, +Row, +ReplaceWith, -NewRow)
-replace_in_col(0, [_|Rest], ReplaceWith, [ReplaceWith|Rest]).
-
-replace_in_col(ColIndex, [X|Rest], ReplaceWith, [X|NewRest]) :-
-    ColIndex > 0,
-    NextColIndex is ColIndex - 1,
-    replace_in_col(NextColIndex, Rest, ReplaceWith, NewRest).
 
 %! spread_fire(+Building, +Row, +Col, -NewBuilding)
 spread_fire(Building, Row, Col, NewBuilding) :-
@@ -198,12 +249,55 @@ spread_fire(Building, Row, Col, NewBuilding) :-
     NextRow is Row - 1,
     spread_fire(TempBuilding, NextRow, Col, NewBuilding).
 
+%! check_already_on_bottom_row(+Row)
+% Check if the room is on the bottom row
+% Cant spread water down if already on the bottom row
+check_already_on_bottom_row(Row) :-
+    Row = 3.
+
 %! check_already_on_top_row(+Row)
+% Check if the room is on the top row
+% Cant spread fire up if already on the top row
 check_already_on_top_row(Row) :-
     Row = 0.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Replace rooms with water or fire %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%! replace(+[RowIndex, ColIndex], +Building, +ReplaceWith, -NewBuilding)
+% Replace the room at the row and column calcualted in the 'update_building' predicate with a 0 or 1 (fire or water)
+replace([RowIndex, ColIndex], Building, ReplaceWith, NewBuilding) :-
+    replace_in_row(RowIndex, ColIndex, Building, ReplaceWith, NewBuilding).
+
+%! replace_in_row(+RowIndex, +ColIndex, +Building, +ReplaceWith, -NewBuilding)
+% Replace the room in row calcualted in the 'update_building' predicate with a 0 or 1 (fire or water)
+replace_in_row(0, ColIndex, [Row|Rest], ReplaceWith, [NewRow|Rest]) :-
+    replace_in_col(ColIndex, Row, ReplaceWith, NewRow).
+
+replace_in_row(RowIndex, ColIndex, [Row|Rest], ReplaceWith, [Row|NewRest]) :-
+    RowIndex > 0,                                                       % If the row index is greater than 0 (not at the top)
+    NextRowIndex is RowIndex - 1,                                       % Decrement the row index (move to the next row)
+    replace_in_row(NextRowIndex, ColIndex, Rest, ReplaceWith, NewRest). % Loop back to replace the room in the next row
+
+%! replace_in_col(+ColIndex, +Row, +ReplaceWith, -NewRow)
+% Replace the room in column calcualted in the 'update_building' predicate with a 0 or 1 (fire or water)
+replace_in_col(0, [_|Rest], ReplaceWith, [ReplaceWith|Rest]).
+
+replace_in_col(ColIndex, [X|Rest], ReplaceWith, [X|NewRest]) :-
+    ColIndex > 0,                                                       % If the column index is greater than 0 (not at the top) 
+    NextColIndex is ColIndex - 1,                                       % Decrement the column index (move to the next column)
+    replace_in_col(NextColIndex, Rest, ReplaceWith, NewRest).           % Loop back to replace the room in the next column
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Start game on load %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
 :- play.
 
 % :-  spread_water([[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], 1, 1, NewBuilding), format('Building: ~w~n', [NewBuilding]),
 %     spread_fire([[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]], 3, 0, BrandNewBuilding), format('Building: ~w~n', [BrandNewBuilding]).
+
+
+% % Test cases
+% test :-
+%     buildingOne(BuildingOne),
+%     buildingTwo(BuildingTwo),
+%     whos_turn(Player),
+%     take_turn(Player, BuildingOne, BuildingTwo).
